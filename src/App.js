b14015ec3c6e7ce35ba69aa814db765eb4741ad8
gaurav <gauravsoni8414@gmail.com>
2023-05-05 16:31:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LeftSide from './Components/LeftSide/LeftSide'
+import RightSide from './Components/RightSide/RightSide'
+
+const App = () => {
+  return (
+    <main>
+    <LeftSide/>
+    <RightSide/>
+    
+    </main>
+  )
+}
+
+export default App
