6eaa51b3ffe828237099e671ada0e9b774b5412e
gaurav <gauravsoni8414@gmail.com>
2023-05-07 08:20:29 +0530

    map added in filter

--- src/Components/MainContent/Portfolio/Portfolio.jsx
+++ src/Components/MainContent/Portfolio/Portfolio.jsx
@@ -5,105 +5,49 @@ import ProjectList from "../../../DataMap/Projects";
 const Portfolio = () => {
   return (
     <article className="portfolio" data-page="portfolio">
-    <header>
-      <h2 className="h2 article-title">Portfolio</h2>
-    </header>
-    <section className="projects">
-  
-      {/*------------ all projects here to show --------------*/}
-      <div className="all-projects">
-        {/* Project 1 */}
-        <div className="project_box  service-item ">
-          <div className="project_img1 project_img">
-          </div>
-          <div className="project-desc">
-            <h2>Google Keep Clone</h2>
-            <p className="timeline-text">Designed the exact user interface and
-              developed this clone using ReactJs. This is a web-based note-taking app that allows users to create,
-              edit, and organize notes.</p>
-            <br />
-            <h4>Features</h4>
-            <div className="project_features_box">
-              <p className="project_features timeline-text">✯
-                Users can create new notes with a title and a body.
-              </p>
-            </div>
-            <div className="project_features_box">
-              <p className="project_features timeline-text">✯
-                Users can edit and delete existing notes.
-              </p>
-            </div>
-            <div className="project-link-btns">
-              <a target="_blank" href="https://googlekeepapp.netlify.app/">
-                <button className="form-btn"> <ion-icon name="eye-outline" /> Live Preview</button>
-              </a>
-              <a target="_blank" href="https://github.com/gauravsoni97">
-                <button className="form-btn"> <ion-icon name="code-slash-outline" /> Source Code</button>
-              </a>
-            </div>
-          </div>
+      <header>
+        <h2 className="h2 article-title">Portfolio</h2>
+      </header>
+      <section className="projects">
+        {/*------------ all projects here to show --------------*/}
+        <div className="all-projects">
+          {(ProjectList || []).map((ele) => {
+            const { id, image, tag, title, description, liveLink, githubLink } =
+              ele;
+            return (
+              <>
+                <div className="project_box  service-item " key={id}>
+                  <div
+                    className="project_img1 project_img"
+                    style={{ backgroundImage: `URL(${image})` }}
+                  >
+        
+
+                  </div>
+                  <div className="project-desc">
+                    <h2>{title}</h2>
+                    <p className="timeline-text">{description}</p>
+                    <br />
+                    <div className="project-link-btns">
+                      <a target="_blank" href={liveLink}>
+                        <button className="form-btn">
+                          <ion-icon name="eye-outline" /> Live Preview
+                        </button>
+                      </a>
+                      <a target="_blank" href={githubLink}>
+                        <button className="form-btn">
+                          <ion-icon name="code-slash-outline" /> Source Code
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
-        {/* Project 2 */}
-        <div className="project_box  service-item ">
-          <div className="project_img project_img2">
-          </div>
-          <div className="project-desc">
-            <h2>Money Management</h2>
-            <p className="timeline-text">
-              The Money Management tool is a web-based application that helps users manage their finances based on
-              the 50-30-20 rule of budgeting.
-            </p>
-            <br />
-            <h4>Features</h4>
-            <div className="project_features_box">
-              <p className="project_features timeline-text"> ✯
-                Users can edit and delete existing notes.
-              </p>
-            </div>
-            <div className="project_features_box">
-              <p className="project_features timeline-text"> ✯
-                Data storage to keep track of user's financial history and progress over time
-              </p>
-            </div>
-            <div className="project-link-btns">
-              <a target="_blank" href="https://moneymanagement.netlify.app/">
-                <button className="form-btn"> <ion-icon name="eye-outline" /> Live Preview</button>
-              </a>
-              <a target="_blank" href="https://github.com/gauravsoni97">
-                <button className="form-btn"> <ion-icon name="code-slash-outline" /> Source Code</button>
-              </a>
-            </div>
-          </div>
-        </div>
-        {/* Project 3 */}
-        <div className="project_box  service-item ">
-          <div className="project_img project_img3">
-          </div>
-          <div className="project-desc">
-            <h2>macOS UI Clone</h2>
-            <p className="timeline-text">
-              Designed and developed the exact user
-              interface of the macOS operating system using
-              ReactJs in which, I have covered Login Page,
-              Docks, New folder, Open Window, Spotlight,
-              Battery Status, Wifi Connection, App menu,
-              Status Menu, Control Center, Apple Menu,
-              Restart Window, Launchpad and a lot more.
-            </p>
-            <br />
-            <div className="project-link-btns">
-              <a target="_blank" href="https://macosui.netlify.app/">
-                <button className="form-btn"> <ion-icon name="eye-outline" /> Live Preview</button>
-              </a>
-              <a target="_blank" href="https://github.com/gauravsoni97">
-                <button className="form-btn"> <ion-icon name="code-slash-outline" /> Source Code</button>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </article>
+      </section>
+    </article>
   );
 };
 
--- src/DataMap/Projects.js
+++ src/DataMap/Projects.js
@@ -1,12 +1,13 @@
- const ProjectList = [
-    {
-        id:1,
-        image:"asdfkj",
-        tag:"HTML",
-        title:"This is Title",
-        description:"This is description",
-        liveLink:"Live Link",
-        githubLink:"github ",
-    }
-]
+const ProjectList = [
+  {
+    id: 1,
+    image: "./projects/googlekeepclone.png",
+    tag: "HTML",
+    title: "Google Keep Clone",
+    description:
+      "Designed the exact user interface and developed this clone using ReactJs. This is a web-based note-taking app that allows users to create, edit, and organize notes.",
+    liveLink: "https://googlekeepapp.netlify.app/",
+    githubLink: "https://github.com/gauravsoni97/googlekeepclone",
+  },
+];
 export default ProjectList;
