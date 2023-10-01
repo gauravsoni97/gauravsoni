f3df55f6fa2cf5b12029fbf37ad79929a26b8a89
gaurav <gauravsoni8414@gmail.com>
2023-10-01 20:09:34 +0530

    toggle menu functionality added for mob

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../index.css";
-
 import myavatar from "../../imgs/my-avatar.png";
 import resume from "../../docs/Gaurav Soni Frontend Developer .pdf";
 import { Link } from "react-router-dom";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-// import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
-import WavingHandIcon from '@mui/icons-material/WavingHand';
-
+import WavingHandIcon from "@mui/icons-material/WavingHand";
 import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
+import { useMediaQuery } from "react-responsive";
 
 const LinkList = [
   {
@@ -59,9 +57,27 @@ const LinkList = [
 ];
 
 const Sidebar = () => {
+  const isMobileDevice = useMediaQuery({ query: "(max-width: 650px)" });
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) {
+      setShowMenu(true);
+    }
+    console.log("useEffect running");
+  }, [isMobileDevice]);
+
   return (
     <>
       <aside className="sidebar">
+        <span className="showMenuIcon" onClick={toggleMenu}>
+          <ExpandMoreRoundedIcon style={{ fontSize: "1.2rem" }} />
+        </span>
         <div className="sidebar-info">
           <figure className="avatar-box">
             <img src={myavatar} alt="Gaurav Soni" width="80px" />
@@ -79,95 +95,96 @@ const Sidebar = () => {
             <p style={{ color: "gray", fontSize: ".8rem" }}>
               Frontend Web Developer
             </p>
-          <div className="sayHiSidebar">
-            <a
-              href={"mailto:gauravsoni8414@gmail.com"}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: ".5rem",
-                color: "#fff",
-              }}
-            >
-              <WavingHandIcon style={{ width: "16px", color:"#ffda6b" }} />
-              Say Hello !
-            </a>
-          </div>
-          </div>
-        </div>
-
-        <div className="sidebar-info_more">
-          <div
-            className="separator"
-            style={{
-              maxWidth: `calc(100% - 2rem)`,
-              margin: "1rem auto",
-            }}
-          ></div>
-
-          <ul className="linklistmenu">
-            {(LinkList || []).map((ele) => {
-              const { id, icon, name, link } = ele;
-              return (
-                <>
-                  <a
-                    target="_blank"
-                    href={link}
-                    style={{
-                      color: "hsla(0, 0%, 84%, 0.7)",
-                      fontSize: ".9rem",
-                      marginLeft: ".8rem",
-                    }}
-                    className="linkname"
-                  >
-                    <span
-                      key={id}
-                      className="links"
-                      style={{
-                        color: "hsl(45, 100%, 71%)",
-                        display: "flex",
-                        alignItems: "center",
-                        margin: ".6rem 0",
-                      }}
-                    >
-                      <span className="icon-style">{icon}</span>
-                      <p
-                        style={{ color: "gray", marginLeft: "1rem" }}
-                        className="inmobText"
-                      >
-                        {name}
-                      </p>
-                    </span>
-                  </a>
-                </>
-              );
-            })}
-          </ul>
-
-          <div
-            className="separator"
-            style={{
-              maxWidth: `calc(100% - 2rem)`,
-              margin: "1rem auto",
-            }}
-          ></div>
-          <div className="info-content downloadresume">
-            <p
-              className="title"
-              style={{ display: "flex", alignItems: "center" }}
-            >
-              <DownloadRoundedIcon style={{ width: "16px" }} />
+            <div className="sayHiSidebar">
               <a
-                href={resume}
-                download
-                style={{ color: "white", marginLeft: ".5rem" }}
+                href={"mailto:gauravsoni8414@gmail.com"}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: ".5rem",
+                  color: "#fff",
+                }}
               >
-                Download CV
+                <WavingHandIcon style={{ width: "16px", color: "#ffda6b" }} />
+                Say Hello !
               </a>
-            </p>
+            </div>
           </div>
         </div>
+        {!showMenu && (
+          <div className="sidebar-info_more">
+            <div
+              className="separator"
+              style={{
+                maxWidth: `calc(100% - 2rem)`,
+                margin: "1rem auto",
+              }}
+            ></div>
+
+            <ul className="linklistmenu">
+              {(LinkList || []).map((ele) => {
+                const { id, icon, name, link } = ele;
+                return (
+                  <>
+                    <a
+                      target="_blank"
+                      href={link}
+                      style={{
+                        color: "hsla(0, 0%, 84%, 0.7)",
+                        fontSize: ".9rem",
+                        marginLeft: ".8rem",
+                      }}
+                      className="linkname"
+                    >
+                      <span
+                        key={id}
+                        className="links"
+                        style={{
+                          color: "hsl(45, 100%, 71%)",
+                          display: "flex",
+                          alignItems: "center",
+                          margin: ".6rem 0",
+                        }}
+                      >
+                        <span className="icon-style">{icon}</span>
+                        <p
+                          style={{ color: "gray", marginLeft: "1rem" }}
+                          className="inmobText"
+                        >
+                          {name}
+                        </p>
+                      </span>
+                    </a>
+                  </>
+                );
+              })}
+            </ul>
+
+            <div
+              className="separator"
+              style={{
+                maxWidth: `calc(100% - 2rem)`,
+                margin: "1rem auto",
+              }}
+            ></div>
+            <div className="info-content downloadresume">
+              <p
+                className="title"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <DownloadRoundedIcon style={{ width: "16px" }} />
+                <a
+                  href={resume}
+                  download
+                  style={{ color: "white", marginLeft: ".5rem" }}
+                >
+                  Download CV
+                </a>
+              </p>
+            </div>
+          </div>
+        )}
       </aside>
     </>
   );
