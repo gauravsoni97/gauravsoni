4f3b34effab4ff7373c3b523b2c7f375513fc26a
gaurav <gauravsoni8414@gmail.com>
2023-10-01 15:48:37 +0530

    skills udpate with map

--- src/Components/MainContent/About/Skills.jsx
+++ src/Components/MainContent/About/Skills.jsx
@@ -1,150 +1,30 @@
-import React from 'react'
-import html from "../../../imgs/skills/Html.png"
-import css from "../../../imgs/skills/Css.png"
-import js from "../../../imgs/skills/Javascript.png"
-import bootstrap from "../../../imgs/skills/Bootstrap.png"
-import sass from "../../../imgs/skills/Sass.png"
-import tailwind from "../../../imgs/skills/Tailwind.jpeg"
-import figma from "../../../imgs/skills/Figma.png"
-import wordpress from "../../../imgs/skills/Wordpress.png"
-import react from "../../../imgs/skills/React.png"
+import React from "react";
+import SkillsData from "../../../DataMap/Skills";
 
 const Skills = () => {
-  
   return (
     <section className="skill">
-        <h3 className="h3 skills-title">Tech Stacks I Use</h3>
-        <ul className="skills-list content-card">
+      <h3 className="h3 skills-title">Tech Stacks I Use</h3>
+      <ul className="skills-list content-card">
+        {SkillsData.map((skill) => (
+          <li className="skills-item" key={skill.id}>
+            <figure className="skills_avatar">
+              <img
+                src={skill.SkillImage}
+                alt={skill.SkillName}
+                width={50}
+                data-testimonials-avatar
+              />
+            </figure>
+            <div className="skills-content-box">
+              <h4 className="h4 skills-item-title">{skill.SkillName}</h4>
+              <p className="skills-item-text">{skill.SkillType}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={html}
-                alt="HTML"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">HTML</h4>
-              <p className="skills-item-text">Mark-Up</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={css}
-                alt="CSS"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">CSS</h4>
-              <p className="skills-item-text">Style Sheet</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={js}
-                alt="JavaScript"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">JavaScript</h4>
-              <p className="skills-item-text">Programming</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={bootstrap}
-                alt="Bootstrap"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">Bootstrap</h4>
-              <p className="skills-item-text">CSS Framework</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-               src={react}
-                alt="React"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">React Js</h4>
-              <p className="skills-item-text">Js Library</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={figma}
-                alt="Figma"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">Figma</h4>
-              <p className="skills-item-text">Designing Tool</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={tailwind}
-                alt="Tailwind"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">Tailwind CSS</h4>
-              <p className="skills-item-text">CSS Framework</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-               src={sass}
-                alt="Sass"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">Sass</h4>
-              <p className="skills-item-text">CSS Preprocessor</p>
-            </div>
-          </li>
-          <li className="skills-item">
-            <figure className="skills_avatar">
-              <img
-                src={wordpress}
-                alt="Wordpress"
-                width={50}
-                data-testimonials-avatar
-              />
-            </figure>
-            <div className="skills-content-box">
-              <h4 className="h4 skiills-item-title">Wordpress</h4>
-              <p className="skills-item-text">CMS</p>
-            </div>
-          </li>
-        </ul>
-      </section>
-  )
-}
-
-export default Skills
+export default Skills;
---
+++ src/DataMap/Skills.js
@@ -0,0 +1,68 @@
+import html from "../imgs/skills/Html.png";
+import css from "../imgs/skills/Css.png";
+import js from "../imgs/skills/Javascript.png";
+import bootstrap from "../imgs/skills/Bootstrap.png";
+import sass from "../imgs/skills/Sass.png";
+import tailwind from "../imgs/skills/Tailwind.jpeg";
+import figma from "../imgs/skills/Figma.png";
+import wordpress from "../imgs/skills/Wordpress.png";
+import react from "../imgs/skills/React.png";
+
+const SkillsData = [
+  {
+    id: 1,
+    SkillName: "HTML",
+    SkillType: "Mark-Up",
+    SkillImage: html,
+  },
+  {
+    id: 2,
+    SkillName: "CSS",
+    SkillType: "Style Sheet",
+    SkillImage: css,
+  },
+  {
+    id: 3,
+    SkillName: "JavaScript",
+    SkillType: "Programming",
+    SkillImage: js,
+  },
+  {
+    id: 4,
+    SkillName: "Bootstrap",
+    SkillType: "CSS Framework",
+    SkillImage: bootstrap,
+  },
+  {
+    id: 5,
+    SkillName: "React Js",
+    SkillType: "Js Library",
+    SkillImage: react,
+  },
+  {
+    id: 6,
+    SkillName: "Figma",
+    SkillType: "Designing Tool",
+    SkillImage: figma,
+  },
+  {
+    id: 7,
+    SkillName: "Tailwind CSS",
+    SkillType: "CSS Framework",
+    SkillImage: tailwind,
+  },
+  {
+    id: 8,
+    SkillName: "Sass",
+    SkillType: "CSS Preprocessor",
+    SkillImage: sass,
+  },
+  {
+    id: 9,
+    SkillName: "Wordpress",
+    SkillType: "CMS",
+    SkillImage: wordpress,
+  },
+];
+
+export default SkillsData;
